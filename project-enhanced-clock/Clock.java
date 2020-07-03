9591d66f741753f508279fc4fdb59409dceee167
Ian McCamey <mccamey.9@osu.edu>
2020-07-03 13:20:15 -0400

    Update Clock.java

--- project-enhanced-clock/Clock.java
+++ project-enhanced-clock/Clock.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 /**
- * A simple class used to demonstrate the implemenation of Java classes.
+ * A simple program to demonstrate the implemenation of Java classes.
  *
- * @author Ian McCamey, Hao Chen Xing
+ * @author Ian McCamey
  * @version 8/26/2019
  */
 
@@ -15,10 +15,8 @@ public class Clock {
     private int day;
     private int month;
     private int year;
-
-    /**
-     * Simple no-argument constructor
-     */
+    
+    // No-argument constructor
     public Clock() {
         this.hour = 1;
         this.minute = 0;
@@ -28,13 +26,8 @@ public class Clock {
         this.month = 1;
         this.year = 2000;
     }
-    /**
-     * Constructor with time arguments
-     * @param hour
-     * @param minute
-     * @param second
-     * @param am
-     */
+    
+    // Constructor with time arguments
     public Clock (int hour, int minute, int second, boolean am) {
     	if(setTime(hour, minute, second, am)) {
     		this.hour = hour;
@@ -46,13 +39,8 @@ public class Clock {
             this.year = 2000;
     	}
     }
-     /**
-      * Constructor with date arguments
-      * @param day
-      * @param month
-      * @param year
-      * @param hour
-      */
+    
+    // Constructor with date arguments
     public Clock (int day, int month, int year) {
     	this.hour = 1;
     	this.minute = 0;
@@ -62,61 +50,41 @@ public class Clock {
     	this.month = month;
     	this.year = year;
     }
-
-    /**
-     * Returns a true/false about whether the set time is valid
-     */
-    public boolean setTime(int hour, int minute, int second, boolean am) {
-    	if(hour > 12 || minute > 59 || second > 59) {
-    		return false;
-    	}
-    	else {
-    		return true;
-    	}
-    }
     
-    /**
-     * Returns a true/false about whether the set date is valid
-     * @param year
-     * @param month
-     * @param day
-     * @return
-     */
+    // Returns a true/false about whether the set time is valid
+    public boolean setTime(int hour, int minute, int second, boolean am) {
+    	if(hour > 12 || minute > 59 || second > 59)
+    		return false;
+    	else
+    		return true;
+    }
+   
+    // Returns a true/false about whether the set date is valid
     public boolean setDate(int year, int month, int day) {
     	return true;
     }
 
-    /**
-     * @return the current hour
-     */
+    // Returns the current hour
     public int getHour() {
         return this.hour;
     }
 
-    /**
-     * @return the current minute value
-     */
+    // Returns the current minute value
     public int getMinute() {
         return this.minute;
     }
 
-    /**
-     * @return the current second value
-     */
+    // Returns the current second value
     public int getSecond() {
         return this.second;
     }
 
-    /**
-     * @return true if the clock is showing an AM time, false otherwise
-     */
+    // Returns true if the clock is showing an AM time, false otherwise
     public boolean getAM() {
         return this.am;
     }
 
-    /**
-     * Advances the time stored in the clock by 1 second
-     */
+    // Advances the time stored in the clock by 1 second
     public void tick() {
     	this.second += 1;
     	if(this.second > 59) {
@@ -127,14 +95,12 @@ public class Clock {
     		this.minute = 0;
     		this.hour += 1;
     	}
-    	if(this.hour > 12) {
+    	if(this.hour > 12)
     		this.hour = 1;
-    	}
     	if(this.hour == 12 && this.minute == 0 && this.second == 0) {
     		this.am = !this.am;
-    		if(this.am) {
+    		if(this.am)
     			this.day += 1;
-    		}
     	}
     	if(this.day > monthDays(this.month)) {
     		if(this.month == 12 ) {
@@ -148,6 +114,7 @@ public class Clock {
     	}
     }
     
+    // Returns the number of days in the given month
     public int monthDays(int month) {
     	switch(month) {
     		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
@@ -159,81 +126,60 @@ public class Clock {
     	}
     }
     
-    
-    /**
-     *
-     * @return the current time and date as a String in YYYY-MM-DD hh:mm:ssAM format
-     */
-    @Override
+    // Returns the current time and date as a String in YYYY-MM-DD hh:mm:ssAM format
     public String toString() {
     	String result = "";
         result += this.year + "-";
-        if(this.month < 10) {
+        if(this.month < 10) 
         	result += "0" + this.month + "-";
-    	}else {
+    	else 
     		result += this.month + "-";
-    	}
-    	if(this.day < 10) {
+    	if(this.day < 10)
     		result += "0" + this.day + " ";
-    	}else {
+    	else
     		result += this.day + " ";
-    	}
-    	if(this.hour < 10) {
+    	if(this.hour < 10)
         	result += "0" + this.hour + ":";
-        }else {
+        else
         	result += this.hour + ":";
-        }
-        if(this.minute < 10) {
+        if(this.minute < 10)
         	result += "0" + this.minute + ":";
-        }else {
+        else 
         	result += this.minute + ":";
-        }
-        if(this.second < 10) {
+        if(this.second < 10)
         	result += "0" + this.second;
-       	}else {
+       	else
         	result += this.second;
-       	}
-        if(this.am) {
+        if(this.am)
         	result += "AM";
-        }else {
+        else
         	result += "PM";
-        }
         return result;
     }
      
-    /**
-      * @return military time in string
-      */
+    // Returns military time in string
     public String militaryString() {
     	String result = "";
     	if(this.am) {
-    		if(this.hour < 10) {
+    		if(this.hour < 10)
     			result = "0" + this.hour;
-    		}
-    		else {
+    		else
     			result += this.hour;
-    		}
-    	}else {
+    	}else
     		result += (this.hour + 12);
-    	}
-    	if(this.minute < 10) {
+    	if(this.minute < 10)
     		result += "0" + this.minute;
-    	}
-    	else {
+    	else
     		result += this.minute;
-    	}
     	return result;
     }
     
-    /**
-     * @return military time in integer
-     */
+    // Returns military time in integer
     public int militaryInt() {
     	String militaryString = "";
     	militaryString += this.hour;
-    	if(this.minute < 10){
+    	if(this.minute < 10)
     		militaryString += 0;
-    	}
     	militaryString += this.minute;
     	int result = Integer.parseInt(militaryString);
     	return result;
